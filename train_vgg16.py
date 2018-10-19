8a73ea0e4dee8b00daa5154f6f71e4445403c78a
Felix <felix.andabasabe@ucdconnect.ie>
2018-10-19 14:11:30 +0100

    add vgg16 layer

---
+++ train_vgg16.py
@@ -0,0 +1,98 @@
+import cv2
+import pickle
+import os.path
+import numpy as np
+from keras.applications import VGG16
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+from keras.models import Sequential
+from keras.layers.convolutional import Conv2D, MaxPooling2D
+from keras.layers.core import Flatten, Dense
+from helpers import resize_to_fit
+
+
+directory_path = os.path.expanduser('~/Documents/research/cropped_aligned/')
+MODEL_FILENAME = "age_estimation_vgg16.hdf5"
+MODEL_LABELS_FILENAME = "model_labels_age_estimation.dat"
+
+
+# initialize the data and labels
+data = []
+labels = []
+
+epochs = 10
+image_size = 150
+
+# loop over the input images
+
+for root, dirs, files in os.walk(directory_path):
+    for filename in files:
+
+        file_name_no_extension, file_extension = os.path.splitext(filename)
+
+        if file_extension == '.jpg' or file_extension == '.jpeg' or file_extension == '.png':
+            full_path = os.path.join(root, filename)
+
+            image = cv2.imread(full_path)
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+            # Resize the letter so it fits in a 20x20 pixel box
+            #image = resize_to_fit(image, 20, 20)
+            image = resize_to_fit(image, image_size, image_size)
+
+            # Add a third channel dimension to the image to make Keras happy
+            image = np.expand_dims(image, axis=2)
+
+            label = int(os.path.basename(root))
+
+            # Add the letter image and it's label to our training data
+            data.append(image)
+            labels.append(label)
+
+
+# scale the raw pixel intensities to the range [0, 1] (this improves training)
+data = np.array(data, dtype="float") / 255.0
+labels = np.array(labels)
+
+# Split the training data into separate train and test sets
+(X_train, X_test, Y_train, Y_test) = train_test_split(data, labels, test_size=0.3, random_state=0)
+
+# Convert the labels (letters) into one-hot encodings that Keras can work with
+lb = LabelBinarizer().fit(Y_train)
+Y_train = lb.transform(Y_train)
+Y_test = lb.transform(Y_test)
+
+# Save the mapping from labels to one-hot encodings.
+# We'll need this later when we use the model to decode what it's predictions mean
+with open(MODEL_LABELS_FILENAME, "wb") as f:
+    pickle.dump(lb, f)
+
+# Build the neural network!
+vgg_conv = VGG16(weights='imagenet', include_top=False, input_shape=(image_size,image_size, 3))
+
+for layer in vgg_conv.layers[:-4]:
+    layer.trainable = False
+
+# Create the model
+model = Sequential()
+
+# Add the vgg convolutional base model
+model.add(vgg_conv)
+
+# Hidden layer with 500 nodes
+model.add(Flatten())
+model.add(Dense(1024, activation="relu"))
+
+# Output layer with 32 nodes (one for each possible letter/number we predict)
+model.add(Dense(17, activation="softmax"))
+
+# Ask Keras to build the TensorFlow model behind the scenes
+model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+
+# Train the neural network
+model.fit(X_train, Y_train, validation_data=(X_test, Y_test), batch_size=32, epochs=epochs, verbose=1)
+
+# Save the trained model to disk
+model.save(MODEL_FILENAME)
+
+print('done')
